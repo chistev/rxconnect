447e0dc85aa615ea087804dcef8ab23a75ec0bf9
chistev <stephenowabie@gmail.com>
2024-11-26 16:49:36 +0100

    added a notifications.ts to modularize the feed/+page.svelte

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -2,54 +2,10 @@
 	import ContactList from "$lib/components/feed/ContactList.svelte";
 	import FeedInput from "$lib/components/feed/FeedInput.svelte";
   import NotificationsAndProfile from "$lib/components/feed/NotificationsAndProfile.svelte";
+  import { notifications } from "$lib/notifications";
+  import type { Notification } from "$lib/notifications";
 
   let showNotifications = false;
-
-  interface Notification {
-    id: number;
-    text: string;
-    time: string;
-    viewed: boolean;
-    profilePic: string;
-  }
-
-  let notifications: Notification[] = [
-    { 
-      id: 1, 
-      text: "Nnaemeka Valentine likes your post: \"Don't attribute to malice what can be...\"", 
-      time: "2w", 
-      viewed: false, 
-      profilePic: "/eminem.jpg" 
-    },
-    { 
-      id: 2, 
-      text: "Raymond Mezieaneke likes your post: \"I've got big dreams for me, man. Nobody...\"", 
-      time: "3w", 
-      viewed: true, 
-      profilePic: "/eminem.jpg" 
-    },
-    { 
-      id: 3, 
-      text: "Princess Nite reacted to your comment: \"Princess Nite\".", 
-      time: "4w", 
-      viewed: false, 
-      profilePic: "/eminem.jpg" 
-    },
-    { 
-      id: 4, 
-      text: "Ogaga Michael Oghenegare commented on your post.", 
-      time: "4w", 
-      viewed: true, 
-      profilePic: "/eminem.jpg" 
-    },
-    { 
-      id: 5, 
-      text: "Nahum Oscar Cfc likes your post: \"Play stupid games, win stupid prizes.\"", 
-      time: "4w", 
-      viewed: true, 
-      profilePic: "/eminem.jpg" 
-    }
-  ];
 
   function handleNotificationClick(notification: Notification) {
     notification.viewed = true;
